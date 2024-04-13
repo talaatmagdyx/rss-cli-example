44e6ec17f3905e5874279990b407c3824e6d121c
talaatmagdyx <talaatmagdy75@gmail.com>
2024-04-14 00:24:42 +0200

    add pretty display for html code

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'rss_feed_plus'
 gem 'nokogiri'
+gem 'tty-markdown', '~> 0.7.2'
--- rss_reader.rb
+++ rss_reader.rb
@@ -5,25 +5,53 @@ require 'nokogiri'
 require 'optparse'
 require 'json'
 require 'cgi'
+require 'tty-markdown'
 
 # Method to display hash data with HTML entity decoding
 def display_hash_data(hash, level = 0)
-    hash.each do |key, value|
-      if value.is_a?(Hash)
-        puts "#{' ' * (level * 2)}#{key}:"
-        display_hash_data(value, level + 1)
-      elsif value.is_a?(Array)
-        value.each_with_index do |item, index|
-          puts "#{' ' * (level * 2)}#{key} #{index + 1}:"
-          display_hash_data(item, level + 1)
-          puts ' ' if index < value.length - 1
-        end
+  hash.each do |key, value|
+    # Remove 'values' from the key if it exists at the end
+    key = key.to_s.gsub(/: ?values$/, '')
+
+    # Check the data type of the value
+    if value.is_a?(Hash)
+      # If the value is a hash, print the key and then recurse deeper
+      puts "#{' ' * (level * 2)}#{key}:"
+      display_hash_data(value, level + 1)
+    elsif value.is_a?(Array)
+      # If the value is an array, iterate through each item in the array
+      value.each_with_index do |item, index|
+        puts "#{' ' * (level * 2)}#{key} #{index + 1}:"
+        display_hash_data(item, level + 1)
+      end
+    else
+      # Decode the value
+      decoded_value = CGI.unescapeHTML(value.to_s)
+
+      # Check if the decoded value is HTML (using simple detection of "<" in the string)
+      if decoded_value.include?("<")
+        # If it contains HTML content, use display_html_content
+        display_html_content(decoded_value)
       else
-        decoded_value = CGI.unescapeHTML(value)
+        # Otherwise, just print the key and decoded value
         puts "#{' ' * (level * 2)}#{key}: #{decoded_value}"
       end
     end
   end
+end
+
+# Method to display HTML content using Markdown format
+def display_html_content(html_content)
+  # Decode HTML entities
+  decoded_html = CGI.unescapeHTML(html_content)
+  # Convert HTML to Markdown using TTY::Markdown
+  markdown_content = TTY::Markdown.parse(decoded_html)
+  # Print the Markdown content
+  puts markdown_content
+end
+
+
+
 
 # Define a method to parse command-line options
 def parse_options
